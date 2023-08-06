3d614c9bf74657445d0f0e7080f8fa9fac1de5b9
yusam <yusam@yandex.ru>
2023-08-06 03:10:31 +0300

    #changed

--- src/ClientDemoSdk.php
+++ src/ClientDemoSdk.php
@@ -6,11 +6,11 @@ use YusamHub\Project0001ClientDemoSdk\Tokens\JwtDemoTokenHelper;
 
 class ClientDemoSdk extends BaseClientSdk
 {
-    public function getHome(): ?array
+    public function getApiV1Test(): ?array
     {
         return $this->doAppRequest(
             $this->api::METHOD_GET,
-            '/api/v1',
+            '/api/v1/test',
             get_defined_vars(),
             true
         );
