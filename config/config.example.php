c11d4be7c295abfa4083f84cef79e9f32a7defdf
yusam <yusam@yandex.ru>
2023-08-06 03:03:20 +0300

    #changed

--- config/config.example.php
+++ config/config.example.php
@@ -3,7 +3,7 @@
 return [
     'demo-sdk' => [
         'isDebugging' => true,
-        'baseUrl' => 'http://192.168.0.110:10002',
+        'baseUrl' => 'http://localhost',
         'storageLogFile' => __DIR__ . '/../tmp/curl-ext-debug-demo-sdk.log',
     ],
 ];
